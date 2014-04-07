68a081d67de3f6bb7c2e2d4a88a7c33d896536f7
eldrad <alan@isomerica.net>
2014-04-07 15:06:09 -0400

    cabal file and updated api.

--- src/Test/ProbabilityCheck.hs
+++ src/Test/ProbabilityCheck.hs
@@ -1,10 +1,16 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Test.ProbabilityCheck
        ( TestableDistribution
        ) where
 
 import Test.QuickCheck (Gen)
 
-class TestableDistribution prop where
-  inspect :: Gen prop
+class TestableDistribution (dist a) where
+  inspect :: dist a -> Gen a
+
+
+instance TestableDistribution (Gen a) where
+  inspect = id
 
 
--- src/Test/Tasty/ProbabilityCheck.hs
+++ src/Test/Tasty/ProbabilityCheck.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Test.Tasty.ProbabilityCheck
        ( testProbabilistic
        ) where
@@ -8,6 +10,6 @@ import qualified Test.ProbabilityCheck as PC
 
 
 -- Determins wether or not the distributions a and b are the same with a confidence of c.
-testProbabilistic :: (PC.TestableDistribution a, PC.TestableDistribution b, Ord c, Num c) => TestName -> a -> b -> c -> TestTree
+testProbabilistic :: (PC.TestableDistribution (a x), PC.TestableDistribution (b x), Ord c, Num c) => TestName -> a x -> b x -> c -> TestTree
 testProbabilistic testName distA distB confidence = undefined
 
