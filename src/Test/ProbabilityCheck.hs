5b4db32f7ccdf66a1fcbf7072d5fca062fc1e26a
eldrad <alan@isomerica.net>
2014-04-07 15:18:59 -0400

    Minor changes to comments.

--- src/Test/ProbabilityCheck.hs
+++ src/Test/ProbabilityCheck.hs
@@ -6,9 +6,10 @@ module Test.ProbabilityCheck
 
 import Test.QuickCheck (Gen)
 
+-- this class will have methods other than inspect however inspect
+-- will always be a sufficient minimal instantiation.
 class TestableDistribution dist a where
   inspect :: dist a -> Gen a
-
 
 instance TestableDistribution Gen a where
   inspect = id
--- src/Test/Tasty/ProbabilityCheck.hs
+++ src/Test/Tasty/ProbabilityCheck.hs
@@ -9,7 +9,8 @@ import Test.Tasty.Options
 import qualified Test.ProbabilityCheck as PC
 
 
--- Determins wether or not the distributions a and b are the same with a confidence of c.
+-- Determins wether or not the distributions a and b are the same with
+-- a confidence of c.
 testProbabilistic :: (PC.TestableDistribution a x, PC.TestableDistribution b x, Ord c, Num c) => TestName -> a x -> b x -> c -> TestTree
 testProbabilistic testName distA distB confidence = undefined
 
