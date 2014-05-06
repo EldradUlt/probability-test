ae3b89e5bdfc62559530e4c1c62888e340ebd167
eldrad <alan@isomerica.net>
2014-05-06 18:07:19 -0400

    Added some basic methods for TestableDistribution and sample. They'll get changed later (hopefully to not include Double). Also some notes to self which will get removed.

--- src/Test/ProbabilityCheck.hs
+++ src/Test/ProbabilityCheck.hs
@@ -2,16 +2,22 @@
 
 module Test.ProbabilityCheck
        ( TestableDistribution
+       , SampleableDistribution
        ) where
 
 import Test.QuickCheck (Gen)
 
 -- this class will have methods other than inspect however inspect
 -- will always be a sufficient minimal instantiation.
-class TestableDistribution dist a where
-  inspect :: dist a -> Gen a
+class TestableDistribution dist where
+  distMean :: s -> Double
+  distVariance :: s -> Double
 
-instance TestableDistribution Gen a where
-  inspect = id
+class SampleableDistribution s where
+  sample :: s -> Gen Double
 
+
+-- N >= ((z - inverse_Phi(1 - beta)) / (mu* / sigma))^2
+
+--wilcoxMatchedPair
 
