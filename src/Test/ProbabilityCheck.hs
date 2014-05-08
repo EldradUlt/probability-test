f4e8e16bf8d014d0dea524cfc7d60a483a34e6c5
eldrad <alan@isomerica.net>
2014-05-08 17:03:48 -0400

    Now have a basic test for checking approximates which all have the same confidence value.

--- src/Test/ProbabilityCheck.hs
+++ src/Test/ProbabilityCheck.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, ScopedTypeVariables #-}
 
 module Test.ProbabilityCheck
        ( TestableDistribution
@@ -6,11 +6,16 @@ module Test.ProbabilityCheck
        , testViaWilcoxMatchedPair
        ) where
 
-import Test.QuickCheck (Gen, generate, vectorOf)
+import Test.QuickCheck (Gen, generate, vectorOf, frequency)
 import qualified Data.Vector.Unboxed as UV
-import Statistics.Test.WilcoxonT (wilcoxonMatchedPairTest, TestType(..), TestResult(..))
+import Statistics.Test.Types (TestType(..), TestResult(..))
+import Statistics.Test.WilcoxonT (wilcoxonMatchedPairTest)
+import Statistics.Test.MannWhitneyU (mannWhitneyUtest)
 import Test.Tasty.HUnit (testCase, (@=?))
 import Test.Tasty (TestTree)
+import Numeric.SpecFunctions (choose)
+import Data.Approximate (Approximate (..))
+import Data.Ratio (numerator, denominator)
 
 -- this class will have methods other than inspect however inspect
 -- will always be a sufficient minimal instantiation.
@@ -28,11 +33,38 @@ class SampleableDistribution s where
 --0-1.
 testViaWilcoxMatchedPair :: Double -> Gen (Double, Double) -> TestTree
 testViaWilcoxMatchedPair alpha genPair =
-  testCase "WilcoxMatchedPair test" (do
-    samples <- generate $ vectorOf sampleSize genPair
-    -- This test will be more clear. Nothing should never
-    -- happen. (Just Insignificant) means the tested pairs were not
-    -- within the desired probability.
-    (Just Significant) @=? (wilcoxonMatchedPairTest TwoTailed alpha (UV.fromList $ map fst samples) (UV.fromList $ map snd samples)))
+  testCase "WilcoxMatchedPair test"
+  (do
+      samples <- generate $ vectorOf sampleSize genPair
+      -- This test will be more clear. Nothing should never
+      -- happen. (Just Insignificant) means the tested pairs were not
+      -- within the desired probability.
+      (Just Significant) @=? (wilcoxonMatchedPairTest TwoTailed alpha (UV.fromList $ map fst samples) (UV.fromList $ map snd samples)))
     where sampleSize = ceiling $ logBase 2 (1/alpha)
+
+-- Obviously inefficient can be replaced.
+smallestCentralBinomialCoefficientGreaterThan :: Double -> Int
+smallestCentralBinomialCoefficientGreaterThan n = helper 1 n
+  where helper a n = if ((2*a) `choose` a) > n then a else helper (a+1) n
+
+-- Note this assumes that all the approximates have the same confidence.
+testSameConfidenceApproximates :: (Ord a) => Double -> Gen ((Approximate a), a) -> TestTree
+testSameConfidenceApproximates p genApprox =
+  testCase "MannWhitneyU test on Approximates"
+  (do
+      (conf, actuals) <- ((generate $ sequence $ take sampleSize $ repeat genApprox) >>= actualsToFirstConfAndActuals)
+      expectedBools <- generate $ vectorOf sampleSize $
+                        frequency [(fromIntegral $ numerator conf, return True)
+                                  , (fromIntegral $ (denominator conf) - (numerator conf), return False)]
+      (Just Significant) @=? (mannWhitneyUtest OneTailed p (boolsToSample expectedBools) (pairsToSample actuals))
+  )
+    where sampleSize = smallestCentralBinomialCoefficientGreaterThan (1/p)
+          getConfidence ((Approximate c _ _ _):_) = toRational c
+          boolsToSample = UV.fromList . (map (fromIntegral . fromEnum))
+          pairToBool ((Approximate _ lo _ hi), a) = a >= lo && a <= hi
+          pairsToSample = boolsToSample . map pairToBool
+          actualsToFirstConfAndActuals :: [(Approximate a, a)] -> IO (Rational, [(Approximate a, a)])
+          actualsToFirstConfAndActuals lst@(((Approximate c _ _ _),_):_) = return (toRational c, lst)
+
+
 
