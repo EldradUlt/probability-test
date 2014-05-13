fac939db9dfe457ddcdc0007530ccf279e49894b
eldrad <alan@isomerica.net>
2014-05-13 16:01:31 -0400

    Now with a basic passing test. When given a Gen that produces pairs of Approximates with 100% confidence and values always inside the the approximate's range the "testSameConfidenceApproximates" correctly always gives them a pass (independant of the confidence value is given to testSameConfidenceApproximates as required).

--- Tests.hs
+++ Tests.hs
@@ -2,10 +2,10 @@ module Main where
 
 import Test.ProbabilityCheck
 import Test.Tasty
-import Test.Tasty.HUnit
+--import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck (testProperty)
 import Data.Approximate (Approximate (..))
-import Test.QuickCheck (choose, Gen)
+import Test.QuickCheck (choose, Gen, arbitrary)
 import Test.QuickCheck.Property (failed, succeeded, Result(..))
 import Statistics.Test.Types (TestResult(..))
 
@@ -15,9 +15,11 @@ main =
   testGroup "probability-test's Tests"
   [
     testProperty "100% Accurate Approximates never fail test." $ do
-       testConfidence <- choose (0,1)
-       testResult <- testSameConfidenceApproximates testConfidence $ (undefined :: Gen (Approximate Integer, Integer))
-         
+       testConfidence <- choose (0,1) >>= (return . (1-))
+       testResult <- testSameConfidenceApproximates testConfidence $ do
+         (high,mid,low) <- (arbitrary :: Gen (Integer, Integer, Integer))
+                           >>= (\(a,b,c) -> return (max a $ max b c, min (max a b) $ min (max b c) (max a c), min a $ min b c))
+         return (Approximate 1 low mid high, mid)
        case testResult of
          Just NotSignificant -> return succeeded
          Just Significant -> return $ failed {reason = "Significant difference between Approximate's confidence and actual success rate."}
