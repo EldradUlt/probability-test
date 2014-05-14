add4ebaac0f5e461e99bd05b5b087c89ef9c7128
eldrad <alan@isomerica.net>
2014-05-14 17:56:52 -0400

    More nuanced tests. Failing more frequently than I think they should.

--- Tests.hs
+++ Tests.hs
@@ -2,12 +2,13 @@ module Main where
 
 import Test.ProbabilityCheck
 import Test.Tasty
---import Test.Tasty.HUnit
+import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck (testProperty)
 import Data.Approximate (Approximate (..))
-import Test.QuickCheck (choose, Gen, arbitrary)
+import Test.QuickCheck (choose, Gen, arbitrary, generate)
 import Test.QuickCheck.Property (failed, succeeded, Result(..))
 import Statistics.Test.Types (TestResult(..))
+import Numeric.Log
 
 main :: IO ()
 main =
@@ -34,7 +35,14 @@ main =
          Just NotSignificant -> return $ failed {reason = "No significant difference between Approximate's confidence and actual success rate."}
          Just Significant -> return succeeded
          Nothing -> return $ failed {reason = "Sample size too small."}
+  , testCase "Low false Positive" $ ((Just NotSignificant) @=?) =<< (generate $ testSameConfidenceApproximates 0.999999 (genApproximate 0.5 0.5))
+    -- This test fails WAY more often than they should.
+  , testCase "Low false Negative" $ ((Just Significant) @=?) =<< (generate $ testSameConfidenceApproximates 0.999999999 (genApproximate 0.75 0.25))
   ]
 
---testSameConfidenceApproximates :: (Ord a) => Double -> Gen ((Approximate a), a) -> Gen (Maybe TestResult)
-
+genApproximate :: Log Double -> Double -> Gen (Approximate Integer, Integer)
+genApproximate assertedConf actualAccuracy = do
+  (low, mid, high) <- (arbitrary :: Gen (Integer, Integer, Integer))
+                      >>= (\(a,b,c) -> return (min a $ min b c, min (max a b) $ min (max b c) (max a c), max a $ max b c))
+  accurate <- choose (0,1) >>= (\a -> return $ a <= actualAccuracy)
+  return $ (Approximate assertedConf low mid high, if accurate then mid else low - 1)
