26e0a9b2db89b8ca59c32ee45e25c5f37c1515a8
eldrad <alan@isomerica.net>
2014-09-10 17:37:26 -0400

    Cleaned up and incremented version number.

--- Tests.hs
+++ Tests.hs
@@ -3,80 +3,36 @@ module Main where
 import Test.ProbabilityCheck
 import Test.Tasty
 import Test.Tasty.HUnit
-import Data.Approximate (Approximate (..))
-import Test.QuickCheck (choose, Gen, arbitrary)
-import Numeric.Log
 import Data.Conduit (($$), Source)
 import qualified Data.Conduit.List as CL
 import System.Random.MWC (withSystemRandom)
 import System.Random.MWC.Distributions (normal)
-import Data.Map.Strict (insertWith)
+import Data.Map.Strict (insertWith, (!))
 
 main :: IO ()
 main =
   defaultMain $
   testGroup "probability-test's Tests"
-  [ {-testGroup "Tests for Same Confidence Approximates"
-    [ testProperty "Correct Approximates claiming 100% accuracy never fails test." $ do
-         testResult <- testSameConfidenceApproximates 0.05 $ genApproximate 1 1
-         case testResult of
-           Just NotSignificant -> return succeeded
-           Just Significant -> return $ failed {reason = "\nSignificant difference between Approximate's confidence and actual success rate."}
-           Nothing -> return $ failed {reason = "Sample size too small."}
-    , testProperty "Incorrect Approximates claiming 100% accuracy always fail test." $ do
-         testResult <- testSameConfidenceApproximates 0.05 $ genApproximate 1 0
-         case testResult of
-           Just NotSignificant -> return $ failed {reason = "\nNo significant difference between Approximate's confidence and actual success rate."}
-           Just Significant -> return succeeded
-           Nothing -> return $ failed {reason = "Sample size too small."}
-    , testCase "Low Type I error" $ ((Just NotSignificant) @=?) =<< (generate $ testSameConfidenceApproximates 0.05 (genApproximate 0.5 0.5))
-    , testCase "Low Type II error" $ ((Just Significant) @=?) =<< (generate $ testSameConfidenceApproximates 0.05 (genApproximate 0.75 0.25))
-    ]
-  , testGroup "Tests for testApproximates"
-    [ testProperty "Correct Approximates with random claimed accuracies never fails test." $ do
-         testResult <- testApproximates 0.05 $ genVerriedApproximate 1
-         case testResult of
-           Just NotSignificant -> return succeeded
-           Just Significant -> return $ failed {reason = "\nSignificant difference between Approximate's confidence and actual success rate."}
-           Nothing -> return $ failed {reason = "Sample size too small."}
-    , testProperty "Incorrect Approximates with random claimed accuracies always fails test." $ do
-         testResult <- testApproximates 0.05 $ genVerriedApproximate (-1)
-         case testResult of
-           Just NotSignificant -> return $ failed {reason = "\nNo significant difference between Approximate's confidence and actual success rate."}
-           Just Significant -> return succeeded
-           Nothing -> return $ failed {reason = "Sample size too small."}
-    , testCase "Consistantly underconfident Approximates should pass a test." $
-      ((Just NotSignificant) @=?) =<< (generate $ testApproximates 0.05 $ genVerriedApproximate 0.1)
-    , testCase "Consistantly overconfident Approximates should fail a test." $
-      ((Just Significant) @=?) =<< (generate $ testApproximates 0.05 $ genVerriedApproximate (-0.1))
-    , testCase "Consistantly accurately confident Approximates shoudl pass a test." $
-      ((Just NotSignificant) @=?) =<< (generate $ testApproximates 0.05 $ genVerriedApproximate 0)
-    ]
-  , -}
-    testGroup "Tests for testNormDistSink"
-    [ testCase "Zero simple case" $ assertResHasVal TestZero $ zeroSource $$ testNormDistSink 0.05 0.05 0.143885
-    , testCase "Positive simple case" $ assertResHasVal TestPositive $ fiveOnehundrethSource $$ testNormDistSink 0.05 0.05 0.143885
+  [ testGroup "Tests for testNormDistSink"
+    [ testCase "Zero simple case" $ assertResHasVal TestZero $ zeroSource $$ testNormDistSink 0.05 0.05
+    , testCase "Positive simple case" $ assertResHasVal TestPositive $ oneTenthSource $$ testNormDistSink 0.05 0.05
     , testCase "100 Samples null is true." $ do
-      lst <- sequence $ replicate 100 $ zeroSource $$ testNormDistSink 0.05 0.05 0.143885
-      assertFailure (show $ foldl dtrFolder (initDTS $ head lst) $ tail lst)
+      lst <- sequence $ replicate 100 $ zeroSource $$ testNormDistSink 0.05 0.05
+      let dts = foldl dtrFolder (initDTS $ head lst) $ tail lst
+        in if (dtsValues dts) ! TestZero >= 85
+           then return ()
+           else assertFailure (show dts)      
+    -- Note the type II error is currently higher than it should be
+    -- when the actual difference is close to the 'minDiff'. This may
+    -- be simply due to not accounting for the estimation of variance.
     , testCase "100 samples null is false." $ do
-      lst <- sequence $ replicate 100 $ fiveOnehundrethSource $$ testNormDistSink 0.05 0.05 0.07194244604317
-      assertFailure (show $ foldl dtrFolder (initDTS $ head lst) $ tail lst)
-    {-
-    , testCase "Passing self test" $ assertResHasVal TestSame $
-               ( (CL.unfoldM (\_ -> do
-                                 res <- zeroSource $$ testNormDistSink 0.05 0.05 0.05
-                                 return $ Just (if TestSame == dtrValue res then ((-0.05) :: Double) else 0.95, ())) ())
-                 $$ testNormDistSink 0.05 0.05 0.01)
-    , testCase "Failing self test" $ assertResHasVal TestSame $
-               ( (CL.unfoldM (\_ -> do
-                                 res <- fiveOnehundrethSource $$ testNormDistSink 0.05 0.05 0.05
-                                 return $ Just (if TestGreater == dtrValue res then (-0.05 :: Double) else 0.95, ())) ())
-                 $$ testNormDistSink 0.05 0.05 0.01)
-    -}
+      lst <- sequence $ replicate 100 $ oneTenthSource $$ testNormDistSink 0.05 0.05
+      let dts = foldl dtrFolder (initDTS $ head lst) $ tail lst
+        in if (dtsValues dts) ! TestPositive >= 85
+           then return ()
+           else assertFailure (show dts)
     ]
   ]
-
 
 dtrFolder :: (Fractional a) => DistributionTestSummary a -> DistributionTestResult a -> DistributionTestSummary a
 dtrFolder (DistributionTestSummary sVals sMeans sStdDevs sCounts sUppers sLowers)
@@ -101,20 +57,5 @@ normalDoubleSource mean = CL.unfoldM (\_ -> do
 zeroSource :: Source IO Double
 zeroSource = normalDoubleSource 0
 
-fiveOnehundrethSource :: Source IO Double
-fiveOnehundrethSource = normalDoubleSource 0.143885
-
-genApproximate :: Log Double -> Double -> Gen (Approximate Integer, Integer)
-genApproximate assertedConf actualAccuracy = do
-  (low, mid, high) <- (arbitrary :: Gen (Integer, Integer, Integer))
-                      >>= (\(a,b,c) -> return (min a $ min b c, min (max a b) $ min (max b c) (max a c), max a $ max b c))
-  accurate <- choose (0,1) >>= (\a -> return $ a <= actualAccuracy)
-  return $ (Approximate assertedConf low mid high, if accurate && (not $ actualAccuracy == 0) then mid else low - 1)
-
-genVerriedApproximate :: Double -> Gen (Approximate Integer, Integer)
-genVerriedApproximate bias = do
-  r <- choose (0,1) :: Gen Double
-  genApproximate (fromRational $ toRational r) (r+bias)
-
-
-
+oneTenthSource :: Source IO Double
+oneTenthSource = normalDoubleSource 0.1
--- src/Test/ProbabilityCheck.hs
+++ src/Test/ProbabilityCheck.hs
@@ -1,85 +1,17 @@
 {-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, ScopedTypeVariables, FlexibleContexts #-}
 
 module Test.ProbabilityCheck
-       ( TestableDistribution
-       , SampleableDistribution
-       , testNormDistSink
+       ( testNormDistSink
        , DistributionTestResult(..), DistributionTestValue(..), DistributionTestSummary(..), initDTS
-       , testViaWilcoxMatchedPair
-       , testApproximates
-       , testSameConfidenceApproximates
-       , minSampleSize
-       , updateSSD, initSSD, StreamStdDev, conduitSSD
-       , upperPerOfNormDist
+       , updateSSD, conduitSSD
        ) where
 
-import Test.QuickCheck (Gen, generate, vectorOf, frequency)
-import qualified Data.Vector.Unboxed as UV
-import Statistics.Test.Types (TestType(..), TestResult(..))
-import Statistics.Test.WilcoxonT (wilcoxonMatchedPairSignificant) --wilcoxonMatchedPairTest)
-import Statistics.Test.MannWhitneyU (mannWhitneyUtest)
-import Statistics.Types (Sample)
-import Test.Tasty.HUnit (testCase, (@=?))
-import Test.Tasty (TestTree)
-import Numeric.SpecFunctions (choose)
-import Data.Approximate (Approximate (..))
-import Data.Ratio (numerator, denominator)
-import qualified Data.Vector.Generic as G
-import Data.Function (on)
-import Statistics.Function (sortBy)
-import Data.Ord (comparing)
-import Numeric.Sum (kbn, sumVector)
+import Statistics.Test.Types (TestType(..))
 import Data.Conduit (Sink, Conduit, await, yield)
 import qualified Data.Conduit.List as CL
 import Data.Number.Erf (invnormcdf, InvErf)
 import Control.Monad (void)
 import Data.Map.Strict (Map, singleton)
-
--- this class will have methods other than inspect however inspect
--- will always be a sufficient minimal instantiation.
-class TestableDistribution dist where
-  distMean :: dist -> Double
-  distVariance :: dist -> Double
-
-class SampleableDistribution s where
-  sample :: s -> Gen Double
-
--- N >= ((z - inverse_Phi(1 - beta)) / (mu* / sigma))^2
-
---This is overly restirictive. Additionally it'll ungracefully throw
---an error if p is < 2^-1023. Also p should be restricted to
---0-1.
-testViaWilcoxMatchedPair :: Double -> Gen (Double, Double) -> Gen (Maybe TestResult)
-testViaWilcoxMatchedPair p genPair = do
-  samples <- vectorOf sampleSize genPair
-  return $ wilcoxonMatchedPairTest' OneTailed p (UV.fromList $ map fst samples) (UV.fromList $ map snd samples)
-    where sampleSize = min 1023 ((ceiling $ logBase 2 (1/p)) * 10)
-
-testApproximates ::(Ord a) => Double -> Gen (Approximate a, a) -> Gen (Maybe TestResult)
-testApproximates p genApprox = 
-  testViaWilcoxMatchedPair p $ genApprox >>= pairToGenDoubleP
-    where pairToGenDoubleP (Approximate conf hi _ lo, actual) = return (fromRational $ toRational conf, if lo <= actual && actual <= hi then 1 else 0)
-
--- Obviously inefficient can be replaced.
-smallestCentralBinomialCoefficientGreaterThan :: Double -> Int
-smallestCentralBinomialCoefficientGreaterThan n = helper 1 n
-  where helper a n = if ((2*a) `choose` a) > n then a else helper (a+1) n
-
--- Note this assumes that all the approximates have the same confidence.
-testSameConfidenceApproximates :: (Ord a) => Double -> Gen ((Approximate a), a) -> Gen (Maybe TestResult)
-testSameConfidenceApproximates p genApprox =
-  do
-    (conf, actuals) <- ((sequence $ take sampleSize $ repeat genApprox) >>= actualsToFirstConfAndActuals)
-    expectedBools <- vectorOf sampleSize $
-                     frequency [ (fromIntegral $ numerator conf, return True)
-                               , (fromIntegral $ (denominator conf) - (numerator conf), return False)]
-    return $ mannWhitneyUtest OneTailed p (boolsToSample expectedBools) (pairsToSample actuals)
-      where sampleSize = (10 *) $ smallestCentralBinomialCoefficientGreaterThan (1/p)
-            getConfidence ((Approximate c _ _ _):_) = toRational c
-            boolsToSample = UV.fromList . (map (fromIntegral . fromEnum))
-            pairToBool ((Approximate _ lo _ hi), a) = lo <= a && a <= hi
-            pairsToSample = boolsToSample . map pairToBool
-            actualsToFirstConfAndActuals lst@(((Approximate c _ _ _),_):_) = return (toRational c, lst)
 
 -- This probably wants better naming at some point.
 data DistributionTestResult a = DistributionTestResult
@@ -118,33 +50,27 @@ initDTS (DistributionTestResult val mean stddev size upper lower) =
 -- greater than Za*stdDev/sqrt(sampleSize) where Za is the upper a
 -- percentage point of the standard normal distribution.
 
-testNormDistSink :: (InvErf a, RealFrac a, Ord a, Monad m) => a -> a -> a -> Sink a m (DistributionTestResult a)
-testNormDistSink alpha beta minDiff = do
+testNormDistSink :: (InvErf a, RealFrac a, Ord a, Monad m) => a -> a -> Sink a m (DistributionTestResult a)
+testNormDistSink alpha minDiff = do
   mNext <- await
   case mNext of
     Nothing -> return $ DistributionTestResult TestInsufficientSample 0 0 0 0 0
-    Just n -> testNormDistSink' alpha beta minDiff $ initSSD n
+    Just n -> testNormDistSink' alpha minDiff $ initSSD n
 
-testNormDistSink' :: (InvErf a, RealFrac a, Ord a, Monad m) =>
-                     a -> a -> a -> StreamStdDev a -> Sink a m (DistributionTestResult a)
-testNormDistSink' alpha beta minDiff ssd = do
+testNormDistSink' :: (InvErf a, RealFrac a, Ord a, Monad m) => a -> a -> StreamStdDev a -> Sink a m (DistributionTestResult a)
+testNormDistSink' alpha minDiff ssd = do
   mNext <- await
   case mNext of
     Nothing -> return $ DistributionTestResult
                { dtrValue = TestInsufficientSample, dtrTestedMean = ssdMean ssd, dtrStdDev = ssdStdDev ssd
                , dtrSampleSize = ssdCount ssd, dtrUpperBound = 0, dtrLowerBound = 0}
-    Just next -> if minSampleSize <= count
+    Just next -> if minSampleSize TwoTailed alpha minDiff stdDev <= count
                  then return $ testNormalDistribution alpha stdDev count mean
-                 else testNormDistSink' alpha beta minDiff newSSD
+                 else testNormDistSink' alpha minDiff newSSD
         where newSSD = updateSSD next ssd
               stdDev = ssdStdDev newSSD
               count = ssdCount newSSD
               mean = ssdMean newSSD
-              minSampleSize = max (minSampleSizeTwoTailed alpha beta minDiff stdDev) 5
-                              -- Having minSampleSize >=5 is just a
-                              -- place holder for accurately
-                              -- accounting for estimation of the
-                              -- varriance.
 
 testNormalDistribution :: (InvErf a, Ord a, Integral b) => a -> a -> b -> a -> DistributionTestResult a
 testNormalDistribution alpha stdDev count actualDiff =
@@ -157,23 +83,19 @@ testNormalDistribution alpha stdDev count actualDiff =
                                        , dtrSampleSize = fromIntegral count, dtrUpperBound = upperTest
                                        , dtrLowerBound = lowerTest}
 
-minSampleSize :: (InvErf a, RealFrac a, Integral b) => TestType -> a -> a -> a -> a -> b
+minSampleSize :: (InvErf a, RealFrac a, Integral b) => TestType -> a -> a -> a -> b
 minSampleSize testType = if testType == OneTailed then minSampleSizeOneTailed else minSampleSizeTwoTailed
 
-minSampleSizeOneTailed :: (InvErf a, RealFrac a, Integral b) => a -> a -> a -> a -> b
---minSampleSizeOneTailed alpha beta minDiff stdDev = ceiling $ (((upperPerOfNormDist alpha) - (inverseCumDist (1-beta))) / (minDiff/stdDev))^2
-minSampleSizeOneTailed alpha beta minDiff stdDev = ceiling $ ((upperPerOfNormDist alpha) / (minDiff/stdDev))^2
+minSampleSizeOneTailed :: (InvErf a, RealFrac a, Integral b) => a -> a -> a -> b
+minSampleSizeOneTailed alpha minDiff stdDev = ceiling $ ((upperPerOfNormDist alpha) / (minDiff/stdDev))^(2::Integer)
 
-minSampleSizeTwoTailed :: (InvErf a, RealFrac a, Integral b) => a -> a -> a -> a -> b
+minSampleSizeTwoTailed :: (InvErf a, RealFrac a, Integral b) => a -> a -> a -> b
 minSampleSizeTwoTailed alpha = minSampleSizeOneTailed (alpha/2)
 
 upperPerOfNormDist :: (InvErf a) => a -> a
 upperPerOfNormDist = invnormcdf . (1 - )
 
--- This is called the Probit and can be numerically approximated.
-inverseCumDist :: (InvErf a) => a -> a
-inverseCumDist = invnormcdf
-
+-- This probably all wants to be moved to another file.
 data StreamStdDev a = StreamStdDev
     { ssdCount :: Integer
     , ssdMean :: a
@@ -210,65 +132,4 @@ conduitSSD = do
       yield (initSSD first, first) 
       void $ CL.mapAccum updateSSDPair $ initSSD first
         where updateSSDPair a s = (updateSSD a s, (updateSSD a s, a))
-
-wilcoxonMatchedPairTest' :: TestType -> Double -> Sample -> Sample -> Maybe TestResult
-wilcoxonMatchedPairTest' test p smp1 smp2 =
-  wilcoxonMatchedPairSignificant test (min n1 n2) p
-  $ wilcoxonMatchedPairSignedRank' smp1 smp2
-    where
-      n1 = UV.length smp1
-      n2 = UV.length smp2
-
-wilcoxonMatchedPairSignedRank' :: Sample -> Sample -> (Double, Double)
-wilcoxonMatchedPairSignedRank' a b = (sum' ranks1, negate (sum' ranks2))
-  where
-    (ranks1, ranks2) = splitByTags
-                       $ UV.zip tags (rank ((==) `on` abs) diffs)
-    (tags,diffs) = UV.unzip
-                   $ addTags                   -- Insert both positive and negative 0's and tag everything else positive or negative.
-                   $ UV.span  (/= 0.0)         -- Divide out equal elements
-                   $ sortBy (comparing abs)    -- Sort the differences by absolute difference
-                   $ UV.zipWith (-) a b        -- Work out differences
-    addTags (zeros,others) = (UV.++) (dupAndTag0s zeros) $ UV.map (\x -> (x>0 , x)) others
-    dupAndTag0s zeros
-      | UV.null zeros = UV.empty
-      | otherwise = UV.cons (True, 0) $ UV.cons (False, 0) $ dupAndTag0s $ UV.tail zeros
-  
--- Private data type for unfolding
-data Rank v a = Rank {
-        rankCnt :: {-# UNPACK #-} !Int        -- Number of ranks to return
-    , rankVal :: {-# UNPACK #-} !Double     -- Rank to return
-    , rankNum :: {-# UNPACK #-} !Double     -- Current rank
-    , rankVec :: v a                        -- Remaining vector
-    }
-                
-                -- | Calculate rank of sample. Sample should be already sorted.
-rank :: (G.Vector v a, G.Vector v Double)
-        => (a -> a -> Bool)        -- ^ Equivalence relation
-        -> v a                     -- ^ Vector to rank
-        -> v Double
-rank eq vec = G.unfoldr go (Rank 0 (-1) 1 vec)
-  where
-    go (Rank 0 _ r v)
-      | G.null v  = Nothing
-      | otherwise =
-          case G.length h of
-            1 -> Just (r, Rank 0 0 (r+1) rest)
-            n -> go Rank { rankCnt = n
-                         , rankVal = 0.5 * (r*2 + fromIntegral (n-1))
-                         , rankNum = r + fromIntegral n
-                         , rankVec = rest
-                         }
-          where
-            (h,rest) = G.span (eq $ G.head v) v
-    go (Rank n val r v) = Just (val, Rank (n-1) val r v)
-
-splitByTags :: (G.Vector v a, G.Vector v (Bool,a)) => v (Bool,a) -> (v a, v a)
-splitByTags vs = (G.map snd a, G.map snd b)
-  where
-    (a,b) = G.unstablePartition fst vs
-
-sum' :: (G.Vector v Double) => v Double -> Double
-sum' = sumVector kbn
-
 
