0fdbe81cf9bf393e9117ab843bd88b0a4b5ede89
eldrad <alan@isomerica.net>
2015-01-06 16:47:10 -0500

    Fleshed out needed parts a bit.

--- src/Test/Tasty/ProbabilityCheck.hs
+++ src/Test/Tasty/ProbabilityCheck.hs
@@ -5,12 +5,21 @@ module Test.Tasty.ProbabilistyCheck
        ) where
 
 import Test.Tasty (TestName, TestTree)
+import Data.Number.Erf (InvErf)
 
 import Test.ProbabilityCheck
 
 testProbabilistic :: ProbTestable a => TestName -> a -> TestTree
 testProbabilistic testName testable = undefined
 
-class ProbTestable prop where
-  foobar :: prop -> Bool --placeholder
+class ProbTestable prob where
+  ptSample :: (InvErf a, RealFrac a, Ord a, Show a, MonadIO m) => prob -> Source m a
+  ptAlpha :: (InvErf a, RealFrac a, Ord a, Show a) => prob -> a
+  ptMinDiff :: (InvErf a, RealFrac a, Ord a, Show a) => prob -> MinDiff a
 
+-- There wants to be a basic Data type similar to QuickCheck's Result
+-- which other instances of this use. However it'll need to have a
+-- numeric type due to the non-binary nature of the results.
+
+-- Eventually want to add in proper error handling similar to
+-- QuickCheck's RoseResult though for the moment skipping.
